d5931435a81d07512bbea4a92b357d345a837131
navleorange <navleorange.0718@gmail.com>
2024-12-16 02:36:18 +0900

    add: start screen

--- src/app/aiwolf_nlp_app.py
+++ src/app/aiwolf_nlp_app.py
@@ -1,12 +1,32 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Button
+from textual.containers import Container
+from textual.widgets import Input, Button, Footer
+
 
 class AIWolfNLPApp(App):
+    CSS_PATH = "aiwolf_nlp_app.tcss"
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
-        yield Button("Start", id="start")
-        yield Button.error("Exit", id="exit")
-    
+        yield Container(
+            Input(
+                value="Human",
+                placeholder="Please input player name",
+                type="text",
+                select_on_focus=False,
+                id="player_name",
+            ),
+            Button(label="Start", id="start"),
+            Button(label="Exit", id="exit"),
+            id="main_container",
+            classes="box"
+        )
+        yield Footer()
+
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "exit":
             self.app.exit()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
--- src/app/screen/common.py
+++
@@ -1,5 +0,0 @@
-from app.aiwolf_nlp_app import App, ComposeResult
-from textual.widgets import Digits
-
-class InputScreen(App):
-    pass
