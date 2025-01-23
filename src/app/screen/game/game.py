d4207d6372c27575d881c90a610f1c589de8de4a
navleorange <navleorange.0718@gmail.com>
2025-01-23 21:14:17 +0900

    add: 画像の追加

--- src/app/screen/game/game.py
+++ src/app/screen/game/game.py
@@ -1,31 +1,38 @@
 from __future__ import annotations
 
-from textual.app import ComposeResult
-from textual.screen import Screen
-from textual.containers import Container, HorizontalGroup, VerticalGroup
-from textual.widgets import Input, Button, Label, RichLog
-from pathlib import Path
-from utils.log_info import LogInfo
-from utils.agent_log import AgentLog
-from player.agent import Agent
-
 from configparser import ConfigParser
+from pathlib import Path
 from typing import TYPE_CHECKING
+
+from PIL import Image
+from textual.app import ComposeResult
+from textual.containers import Container, HorizontalGroup, VerticalGroup
+from textual.screen import Screen
+from textual.widgets import Input, Label, RichLog, Static, LoadingIndicator
+from rich_pixels import Pixels
+
+from player.agent import Agent
+from utils.agent_log import AgentLog
+from utils.log_info import LogInfo
 
 if TYPE_CHECKING:
     from aiwolf_nlp_common.client import Client
 
 from aiwolf_nlp_common.client.websocket import WebSocketClient
 
-
 class GameScreen(Screen):
     CSS_PATH = "game.tcss"
+    IMAGE_PATH = "./src/res/images/seer.png"
 
     def __init__(self, user_name: str):
-        self.agent_initialize(user_name=user_name)
+        self._agent_initialize(user_name=user_name)
+
+        self.image = Image.open(self.IMAGE_PATH)
+        self.image = Pixels.from_image(self.image)
+
         super().__init__()
 
-    def agent_initialize(self, user_name: str) -> None:
+    def _agent_initialize(self, user_name: str) -> None:
         config_path = "./src/res/config.ini"
 
         if Path(config_path).exists():
@@ -44,22 +51,31 @@ class GameScreen(Screen):
             name=user_name,
             agent_log=AgentLog(config=config, agent_name=user_name, log_info=log_info),
         )
+    
+    def _create_detail_label(self, key:str, value:str) -> Label:
+        return Label(f"[u][b]{key}: [/b]{value}", classes="detail_info_label")
 
     def compose(self) -> ComposeResult:
         yield HorizontalGroup(
-            Label(f"プレイヤー名:{self.agent.name}"),
-            Label(f"Agent名: {self.agent.index}"),
+            VerticalGroup(
+                Label("[bold]・会話履歴", id="history_log_label"),
+                RichLog(markup=True, id="history_log"),
+                id="history_container",
+            ),
+            Container(
+                Label(id="image"),
+                Container(
+                    self._create_detail_label(key="プレイヤー名", value=self.agent.name),
+                    self._create_detail_label(key="Agent名", value=self.agent.index),
+                    self._create_detail_label(key="役職", value="占い師"),
+                    id="detail_info_container"
+                ),
+                id="player_info_container"
+            ),
+            id="info_container",
         )
-        yield VerticalGroup(
-            Label("会話履歴", id="history_log_label"),
-            RichLog(markup=True, id="history_log"),
-            id="history_container"
-        )
-        yield Container(
-            Input(id="text_input"),
-            id="text_container"
-        )
-    
+        yield Container(Input(id="text_input"), id="text_container")
+
     def _on_mount(self, event):
         rich_log = self.query_one(RichLog)
 
@@ -76,3 +92,4 @@ class GameScreen(Screen):
 
         rich_log.write(text)
 
+        self.query_one("#image",Static).update(self.image)
